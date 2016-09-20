14c686c82d214ff88b919e34a27d87e8b810f779
George Ramirez <george.ramirez2@gmail.com>
2016-09-20 15:17:03 -0500

    google api getting lat lng and printing to screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,67 +1,117 @@
-// add ready function??
-
-alert("working!");
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCJc9HSfoAU52BKbniU98Tb4JaiYPCvxkI",
-    authDomain: "tripmaker-adbb4.firebaseapp.com",
-    databaseURL: "https://tripmaker-adbb4.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "9591901571"
-  };
-  firebase.initializeApp(config);
-
-var database = firebase.database();
-  // =======================================================
-$("#").on("click", function(){
-
-	// Get data from input fields
-	var where =
-	var toDo1 =
-	var toDo2 = 
-	var toDo3 = 
-	var toDo4 =
-	var toDo5 =
-	var eatDrink1 = 
-	var eatDrink2 = 
-	var eatDrink3 =
-	var eatDrink4 = 
-	var distance = ?
-	var idealMonth = ?
-	var temp = ?
-	var contactName = ?
 
 
- var dayTrip = {
- 	// I'm guessing at these for now, just getting the code right
- 	destination: where
+$(document).ready(function() {
 
- 	to_do_1: toDo1
- 	to_do_2: toDo2
- 	to_do_3: toDo3
- 	to_do_4: toDo4
- 	to_do_5: toDo5
+	// alert("working!");
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyCJc9HSfoAU52BKbniU98Tb4JaiYPCvxkI",
+	    authDomain: "tripmaker-adbb4.firebaseapp.com",
+	    databaseURL: "https://tripmaker-adbb4.firebaseio.com",
+	    storageBucket: "",
+	    messagingSenderId: "9591901571"
+	  };
+	  firebase.initializeApp(config);
 
- 	to_consume1: eatDrink1
- 	to_consume2: eatDrink2
- 	to_consume3: eatDrink3
- 	to_consume4: eatDrink4
+	var database = firebase.database();
 
- 	travel_distance: distance
-
- 	ideal_month: idealMonth
-
- 	current_temp: temp
-
- 	contact_name: contactName
+	// ===Need to either make an on click function to grab the value from the input, or make a button====
 
 
- 	/*TIMESTAMP*/
+	// ==================GOOGLE API CALL & Print Default Map to screen=====================
 
- 	};/*end of dayTrip json object*/
+	var Key = 'AIzaSyAxNtwAwM8tjrDJJQQSHfJzgepd1YI54_E';
 
-	database.ref('day_trip/').push(dayTrip)/*Is this correct*/
+	// Google API to get lat/lng or place_id
+	queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=+Austin,+TX&key='+Key;
 
- 	// return false; /*Do I need this?, What was the other method Josh said to use?*/
+	$.ajax({
 
-});/*End of on click funtion*/
+		url: queryURL,
+		method: 'GET'
+		
+		}).done(function(json) {
+				// Do we need to grab to lat long and put them in the data base?
+				console.log(json);
+
+				// console.log(json.results[0].place_id);
+				var placeID = json.results[0].place_id;
+				console.log(placeID);
+
+				// console.log(json.results[0].geometry.location.lat);
+				var placeLat = json.results[0].geometry.location.lat;
+				console.log(placeLat);
+				
+				// console.log(json.results[0].geometry.location.lng);
+				var placeLng = json.results[0].geometry.location.lng;
+				console.log(placeLng);
+				
+				// This is printing to the screen but should be out of this function
+				$("#map2").append('<iframe src=https://www.google.com/maps/embed/v1/place?key='+Key+'&q=austin+tx width="800" height="600" frameborder="0" style="border:0" ></iframe>');
+				
+		});
+
+	// https://www.google.com/maps/embed/v1/place?key=AIzaSyAxNtwAwM8tjrDJJQQSHfJzgepd1YI54_E&q=austin+tx
+
+
+// ================END GOOGLE API CALL=====================
+
+// =================Printing Map To Screen=================
+
+
+//=========================================================
+// $("#").on("click", function(){
+
+// 	// Get data from input fields
+// 	var where =
+// 	var toDo1 =
+// 	var toDo2 = 
+// 	var toDo3 = 
+// 	var toDo4 =
+// 	var toDo5 =
+// 	var eatDrink1 = 
+// 	var eatDrink2 = 
+// 	var eatDrink3 =
+// 	var eatDrink4 = 
+// 	var distance = ?
+// 	var idealMonth = ?
+// 	var temp = ?
+// 	var contactName = ?
+
+
+//  var dayTrip = {
+//  	// I'm guessing at these for now, just getting the code right
+//  	destination: where
+
+//  	to_do_1: toDo1
+//  	to_do_2: toDo2
+//  	to_do_3: toDo3
+//  	to_do_4: toDo4
+//  	to_do_5: toDo5
+
+//  	to_consume1: eatDrink1
+//  	to_consume2: eatDrink2
+//  	to_consume3: eatDrink3
+//  	to_consume4: eatDrink4
+
+//  	travel_distance: distance
+
+//  	ideal_month: idealMonth
+
+//  	current_temp: temp
+
+//  	contact_name: contactName
+
+
+//  	/*TIMESTAMP*/
+
+//  	};/*end of dayTrip json object*/
+
+// 	database.ref('/day_trip').push(dayTrip)/*Is this correct, does that forward slash need to be there?*/
+
+//  	// return false; /*Do I need this?, What was the other method Josh said to use?*/
+
+// });/*End of on click funtion*/
+
+
+});/*END OF .ready function*/
