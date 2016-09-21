a8526161979fea79a19b2a6fcf822af6c55ddfe3
George Ramirez <georgeramirez@georges-mbp.gateway.pace.com>
2016-09-21 09:04:50 -0500

    updating css js file and minor changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -52,9 +52,7 @@ $(document).ready(function() {
 				
 		});
 
-	};/*end of dayTrip json object*/
-
-	database.ref('day_trip/').push(dayTrip)/*Is this correct*/
+	
 
 	// https:www.google.com/maps/embed/v1/place?key=AIzaSyAxNtwAwM8tjrDJJQQSHfJzgepd1YI54_E&q=austin+tx
 
@@ -71,58 +69,53 @@ $(document).ready(function() {
 
 
 //=========================================================
-// $("#").on("click", function(){
+		$("#addActivity").on("click", function(){
 
-// 	// Get data from input fields
-// 	var where =
-// 	var toDo1 =
-// 	var toDo2 = 
-// 	var toDo3 = 
-// 	var toDo4 =
-// 	var toDo5 =
-// 	var eatDrink1 = 
-// 	var eatDrink2 = 
-// 	var eatDrink3 =
-// 	var eatDrink4 = 
-// 	var distance = ?
-// 	var idealMonth = ?
-// 	var temp = ?
-// 	var contactName = ?
-
-
-//  var dayTrip = {
-//  	// I'm guessing at these for now, just getting the code right
-//  	destination: where
-
-//  	to_do_1: toDo1
-//  	to_do_2: toDo2
-//  	to_do_3: toDo3
-//  	to_do_4: toDo4
-//  	to_do_5: toDo5
-
-//  	to_consume1: eatDrink1
-//  	to_consume2: eatDrink2
-//  	to_consume3: eatDrink3
-//  	to_consume4: eatDrink4
-
-//  	travel_distance: distance
-
-//  	ideal_month: idealMonth
-
-//  	current_temp: temp
-
-//  	contact_name: contactName
+			// Get data from input fields
+			var where = test1111;
+			var toDo1 = "";
+			var toDo2 = "";
+			var toDo3 = "";
+			var toDo4 = "";
+			var toDo5 = "";
+			var eatDrink1 = test1111;
+			var eatDrink2 = ""; 
+			var eatDrink3 = "";
+			var eatDrink4 = "";
+			var distance = "";
+			var idealMonth = "";
+			var temp = "";
+			var contactName = "";
 
 
-//  	/*TIMESTAMP*/
+		 var dayTrip = {
+		 	// I'm guessing at these for now, just getting the code right
+		 	destination: where,
+		 	to_do_1: toDo1,
+		 	to_do_2: toDo2,
+		 	to_do_3: toDo3,
+		 	to_do_4: toDo4,
+		 	to_do_5: toDo5,
+		 	to_consume1: eatDrink1,
+		 	to_consume2: eatDrink2,
+		 	to_consume3: eatDrink3,
+		 	to_consume4: eatDrink4,
+		 	travel_distance: distance,
+		 	ideal_month: idealMonth,
+		 	current_temp: temp,
+		 	contact_name: contactName
 
-//  	};/*end of dayTrip json object*/
+		 	/*TIMESTAMP*/
 
-// 	database.ref('/day_trip').push(dayTrip)/*Is this correct, does that forward slash need to be there?*/
+		 	};/*end of dayTrip json object*/
 
- 	// return false; /*Do I need this?, What was the other method Josh said to use?*/
+			database.ref('/day_trip').set(dayTrip)/*Is this correct, does that forward slash need to be there?*/
+			database.ref('/weekend_trip').push(dayTrip)
+			database.ref('/long_trip').push(dayTrip)
 
- //});/*End of on click funtion*/
+		 	// return false; /*Do I need this?, What was the other method Josh said to use?*/
+
+		 });/*End of on click funtion*/
 
 });/*END OF .ready function*/
 
